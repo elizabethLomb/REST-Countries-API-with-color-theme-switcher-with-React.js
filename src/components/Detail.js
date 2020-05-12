a7feeaa6a5169fcabd3cce536f557942fceb230f
elizabethLomb <elizabeth.lombardi7@gmail.com>
2020-05-12 21:19:23 +0200

    object iteration done missing render property

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,25 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Detail = (props) => {
-  
-  const country = Object.values(props.countries).map(c => 
-    c.filter(c => c.name === props.match.params.name && c)
-  )
 
-  const countryBorder = Object.values(props.countries).map(c => (
-    c.filter(el => (
-      country.map((value, i) => (
-        value[i].borders.filter(v => {
-          if(v === el.alpha3Code){
-            return (
-              el.name
-            )
-          }
-        })
-      ))
-    ))
-  ))
- 
+  const country = Object.values(props.countries).map(c => {
+    return c.filter(el => el.name === props.match.params.name && c)
+  })
+
+  // const countryBorder = Object.values(props.countries).map((key, i) => {
+  //   return key.filter(el => {
+  //     return country.map((c, i) => {
+  //       if(c[i].borders === el.alpha3Code) return el.name
+  //     })
+  //   })
+  // })
+
 
   return (
     <main className="container-fluid mt-5 pt-5 countryDetail">
@@ -28,38 +22,38 @@ const Detail = (props) => {
         <Link to='/' className="btn btn-light">← Back</Link>
       </div>
 
-      {country.map((value, i) => (
-        <div className="d-flex align-items-center justify-content-between" key={i}>
+      {country.map((c, i) => {
+        return (
+          <div className="d-flex align-items-center justify-content-between" key={i}>
+            <div className="col-lg-5 col-md-5 col-12 text-right">
+              <img className="flag" src={c[i].flag} alt={c[i].name} />
+            </div>
 
-          <div className="col-lg-5 col-md-5 col-12 text-right">
-            <img className="flag" src={value[i].flag} alt={value[i].name} />
-          </div>
-
-          <div className="col-lg-6 col-md-6 col-12">
-            <h1 className="display-4 mb-4">{value[i].name}</h1>
+            <div className="col-lg-6 col-md-6 col-12">
+              <h1 className="display-4 mb-4">{c[i].name}</h1>
 
               <div className="d-flex justify-content-between no-gutters">
                 <div className="col-lg-6 col-12">
                   <ul className="no-bullets">
                     <li>
                       <span className="font-weight-bold mr-3">Native Name:</span>
-                      {value[i].nativeName}
+                      {c[i].nativeName}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Population:</span>
-                      {value[i].population}
+                      {c[i].population}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Region:</span>
-                      {value[i].region}
+                      {c[i].region}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Sub Region:</span>
-                      {value[i].subregion}
+                      {c[i].subregion}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Capital:</span>
-                      {value[i].capital}
+                      {c[i].capital}
                     </li>
                   </ul>
                 </div>
@@ -67,15 +61,15 @@ const Detail = (props) => {
                   <ul className="no-bullets">
                     <li>
                       <span className="font-weight-bold mr-3">Top Level Domain:</span>
-                      {value[i].topLevelDomain[0]}
+                      {c[i].topLevelDomain[0]}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Currencies:</span>
-                      {value[i].currencies[0].name}
+                      {c[i].currencies[0].name}
                     </li>
                     <li>
                       <span className="font-weight-bold mr-3">Languages:</span>
-                      {value[i].languages[0].name}
+                      {c[i].languages[0].name}
                     </li>
                   </ul>
                 </div>
@@ -83,38 +77,53 @@ const Detail = (props) => {
 
               <div className="d-flex align-items-center">
                 <span className="font-weight-bold mr-4">Border Countries</span>
-                {value[i].borders.length ? (
-                  value[i].borders.map((border, index) => {
-                    return (
-                      <Link key={index} to={`/${border}`}>
-                        {Object.values(props.countries).filter((c, i) => {
-                          console.log(c[i].alpha3Code)
-                          if(c.alpha3Code === border)
+                {c[i].borders.length ? (
+                  c[i].borders.map((border, i) => {
+                    return(
+                      <Link to="/" key={i}>
+                        {Object.keys(props.countries).map(c => {
+                          props.countries[c].filter(el => {
+                            if(el.alpha3Code === border) {
+                              console.log(el.name)
+                              return el.name
+                            }
+                          })
+                          // if(props.countries[c].alpha3Code === border)
+                          // console.log(props.countries[c])
+                          //   return props.countries[c].name
                           
-                            return props.countries[c].name
-                        })}
+                          //if(props.countries[c].alpha3Code === border)
+                            //return props.countries[c].name
+
+
+
+                          // return c.filter((el, i) => {
+                          //   c
+                          //   if(el.alpha3Code === b) return props.countries[el].name
+                          // }
+                          
+                        })
+                        }
                       </Link>
                     )
                   })
+                  // Object.values(props.countries).map(c => {
+                  //   return c.filter(el => {
+                  //     c[i].borders.map((border, i) => {
+                  //       <Link to="/" key={i}>
+                  //       {el.alpha3Code === border && el.name}
+                  //       </Link>
+                  //     })
+                  //   })
+                  // })
                 ) : (
                   <>No borders</>
                 )}
-                {/* {value[i].borders.filter(v => {
-                  return Object.values(props.countries).map(c => {
-                    c.filter((el, i) => {
-                      if(v === el.alpha3Code) {
-                        return el.name
-                      }
-                    })
-                  })
-                })}        */}
               </div>
-
-
+            </div>
           </div>
-
-        </div>
-      ))}      
+        )
+      })}
     </main>
   )
 }
